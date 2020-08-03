c39b900176e7a0ded4a731d49fa07d4b3462d8ad
Noredeen Al-Zu'bi <noredeenalzubi@gmail.com>
2020-08-03 14:13:29 +0300

    Add basic UI to homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :cities
   resources :stunning_views
-  root to: "stunning_views#index"
+  root to: "pages#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

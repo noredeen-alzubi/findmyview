cf8119f0695e239c2f035f59fd2718e1efaa2bf1
Noredeen Al-Zu'bi <noredeenalzubi@gmail.com>
2020-08-03 09:09:08 +0300

    Add and configure Bootstrap.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :cities
   resources :stunning_views
+  root to: "stunning_views#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

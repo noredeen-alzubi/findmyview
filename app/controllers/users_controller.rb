fbcabbed44473fde7d5c1a81fb4fbacf1b145e6d
Noredeen Al-Zu'bi <noredeenalzubi@gmail.com>
2020-08-06 18:24:34 +0300

    Make signup more interactive.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,19 @@ class UsersController < ApplicationController
   end
   def create
     @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to FindMyView #{@user.name}!"
+      redirect_to stunning_views_path
+    else
+      render 'new'
+    end
   end
+
   def update
   end
+
   private
   def user_params
-    params.require(:user).permit(:name, :email, :password)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end 
 end

ee567fd47b4226bf9ccf7d39c550e404e32bffee
Noredeen Al-Zu'bi <noredeenalzubi@gmail.com>
2020-08-06 15:10:14 +0300

    Set up User model for authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_05_074405) do
+ActiveRecord::Schema.define(version: 2020_08_06_073259) do
 
   create_table "cities", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,15 @@ ActiveRecord::Schema.define(version: 2020_08_05_074405) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["city_id"], name: "index_stunning_views_on_city_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "stunning_views", "cities"
